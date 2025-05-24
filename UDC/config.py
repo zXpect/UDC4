5181ddabbd198a180834cc5a37959814c5f6472b
zXpect <zzzxpect2@gmail.com>
2025-05-24 00:21:14 -0500

    Insert Collections

--- UDC/config.py
+++ UDC/config.py
@@ -4,11 +4,13 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     MONGODB_URI = os.environ.get('MONGODB_URI')
     GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
-    SESSION_TYPE = 'filesystem'  
+    SESSION_TYPE = 'filesystem'
     SESSION_PERMANENT = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    # Establecer valor por defecto útil en desarrollo local
+    MONGODB_URI = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/udc_netschool')
 
 class ProductionConfig(Config):
     DEBUG = False
@@ -19,4 +21,5 @@ class ProductionConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    MONGODB_URI = 'mongodb://localhost:5000/test_myapp'
+    # Usar puerto por defecto de MongoDB para pruebas también
+    MONGODB_URI = 'mongodb://localhost:27017/test_myapp'
